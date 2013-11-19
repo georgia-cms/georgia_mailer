ab794f4f86620757324fdc8b5afe3f0483faeb62
Mathieu Gagné <mathieu@motioneleven.com>
2013-11-19 11:59:30 -0500

    Add engine dependencies

--- lib/georgia_mailer/engine.rb
+++ lib/georgia_mailer/engine.rb
@@ -1,5 +1,12 @@
 module GeorgiaMailer
   class Engine < ::Rails::Engine
     isolate_namespace GeorgiaMailer
+
+    require 'sendgrid'
+    require 'rakismet'
+    require 'carrierwave'
+    require 'fog'
+    require 'sunspot_rails'
+
   end
 end
