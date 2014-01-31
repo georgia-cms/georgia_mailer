2f56bf04ef5b85bd2b61978eb173b710dbb9253e
Mathieu Gagne <gagne.mathieu@hotmail.com>
2014-01-31 13:20:00 -0500

    Adds routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 GeorgiaMailer::Engine.routes.draw do
-  resources :messages, only: [:create], path: '/'
+
+  resources :messages do
+    collection do
+      get :search
+      get :destroy_all_spam
+    end
+    member do
+      get :spam
+      get :ham
+    end
+  end
 end
