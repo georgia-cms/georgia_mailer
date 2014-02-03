ba6e240c9ea355e4b3578bd237a5ad36663e1768
Mathieu Gagné <mathieu@motioneleven.com>
2014-02-03 17:00:42 -0500

    Adds Georgia indexer extensions as concerns

--- app/models/georgia_mailer/message.rb
+++ app/models/georgia_mailer/message.rb
@@ -24,36 +24,11 @@ module GeorgiaMailer
       @status ||= spam ? 'spam' : 'clean'
     end
 
-    include Georgia::Indexer
-    # is_searchable :georgia_mailer_message
-
-    include ::Tire::Model::Search
-    include ::Tire::Model::Callbacks
-
-    def to_indexed_json
-      {
-        name: name,
-        email: email,
-        message: message,
-        subject: subject,
-        phone: phone,
-        status: status,
-        created_at: created_at.strftime('%F')
-      }.to_json
-    end
-
-    def self.search model, params
-      model.tire.search(page: (params[:page] || 1), per_page: (params[:per] || 25)) do
-        if params[:query].present?
-          query do
-            boolean do
-              must { string params[:query], default_operator: "AND" }
-            end
-          end
-          sort { by (params[:o] || :created_at), (params[:dir] || :desc) }
-        end
-      end.results
-    end
+    include Georgia::Indexer::Adapter
+    is_searchable({
+      solr: GeorgiaMailer::Concerns::SolrGeorgiaMailerMessageExtension,
+      tire: GeorgiaMailer::Concerns::TireGeorgiaMailerMessageExtension
+    })
 
   end
 end
