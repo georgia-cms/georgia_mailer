a861ab5e5bb5c2803f63b0d061a3a0a367df8eb2
Mathieu Gagne <gagne.mathieu@hotmail.com>
2014-03-20 10:01:41 -0400

    Fixes georgiacms email address

--- app/mailers/georgia_mailer/notifier.rb
+++ app/mailers/georgia_mailer/notifier.rb
@@ -6,8 +6,8 @@ module GeorgiaMailer
       emails_to = Georgia::User.where(receives_notifications: true).map(&:email)
       unless emails_to.empty?
         mail(
-          from: "noreply@georgiacms.com",
-          to: "noreply@georgiacms.com",
+          from: "noreply@georgiacms.org",
+          to: "noreply@georgiacms.org",
           bcc: emails_to,
           subject: "New message from #{Georgia.title}"
           )
