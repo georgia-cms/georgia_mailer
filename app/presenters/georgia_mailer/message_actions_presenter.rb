f399dc9c373d5829fef557a12ed921edbeebefd4
Mathieu Gagne <gagne.mathieu@hotmail.com>
2014-04-04 09:22:10 -0400

    Made message actions single buttons

--- app/presenters/georgia_mailer/message_actions_presenter.rb
+++ app/presenters/georgia_mailer/message_actions_presenter.rb
@@ -8,7 +8,7 @@ module GeorgiaMailer
 
     def to_s
       html = ActiveSupport::SafeBuffer.new
-      html << content_tag(:li, link_to_print)
+      # html << content_tag(:li, link_to_print)
       if spam?
         html << content_tag(:li, link_to_ham) if can?(:ham, @message)
       else
@@ -22,27 +22,27 @@ module GeorgiaMailer
     private
 
     def link_to_print
-      link_to "#{icon_tag('print')} Print".html_safe, "javascript:window.print()", target: '_blank'
+      link_to "#{icon_tag('print')} Print".html_safe, "javascript:window.print()", target: '_blank', class: 'btn btn-warning btn-sm'
     end
 
     def link_to_reply
-      link_to "#{icon_tag('reply')} Reply".html_safe, "mailto:#{@message.email}", target: '_blank'
+      link_to "#{icon_tag('reply')} Reply".html_safe, "mailto:#{@message.email}", target: '_blank', class: 'btn btn-warning btn-sm'
     end
 
     def link_to_ham
-      link_to "#{icon_tag('thumbs-up')} Mark as ham".html_safe, [:ham, @message]
+      link_to "#{icon_tag('thumbs-up')} Move to Inbox".html_safe, [:ham, @message], class: 'btn btn-warning btn-sm'
     end
 
     def link_to_spam
-      link_to "#{icon_tag('thumbs-down')} Mark as spam".html_safe, [:spam, @message]
+      link_to "#{icon_tag('thumbs-down')} Report spam".html_safe, [:spam, @message], class: 'btn btn-warning btn-sm'
     end
 
     def link_to_resend_notification
-      link_to "#{icon_tag('envelope')} Resend notification".html_safe, [:resend_notification, @message]
+      link_to "#{icon_tag('envelope')} Resend notification".html_safe, [:resend_notification, @message], class: 'btn btn-warning btn-sm'
     end
 
     def link_to_trash
-      link_to "#{icon_tag('trash-o')} Trash".html_safe, @message, method: :delete, data: {confirm: 'Are you sure?'}
+      link_to "#{icon_tag('trash-o')} Trash".html_safe, @message, method: :delete, data: {confirm: 'Are you sure?'}, class: 'btn btn-danger btn-sm'
     end
 
     def spam?
